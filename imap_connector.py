65ffdcb2ea9cccd049d69e871706bb0dafa34916
Ilya Podolyako <ip@runport.io>
2022-02-23 10:43:48 -0500

    ImapConnector retrieves messages.:

--- imap_connector.py
+++ imap_connector.py
@@ -28,66 +28,136 @@ import constants
 import observ2
 
 # 3) Data
-# N/a
+ALL = "ALL"
+BATCH = 10
+READ_ONLY = True
+STANDARD_OFFSET = -40
 
 # 4) Functions
 class ImapConnector:
-    def __init__(self, url, guest):
+    def __init__(self, url, guest, batch=BATCH):
         self._account = guest
-        self._offset = 0
+        self._offset = STANDARD_OFFSET
         self._service = url
         self._session = None
+
+    def check_unread(self):
+        # get count
+        # subtract offset?
+        # probably want to get the uids for those? or not really?
+        # return result?
+        pass
     
-    def check(self, count=10, offset=None, trace=False):
-        uniques = self.refresh()
-
-        if offset is None:
-            offset = self.get_offset()
-        requested_uniques = uniques[offset: (offset+count)]
-        # pad with Nones? 
-        # for i in uniques[offset: x]:
-        #   get message
-        #   increment i so that i know where i stopped
-        
-        messages = list()
-        for u in requested_uniques:
-            message = self._get_message(u)
-            messages.append(message)
-
-        new_offset = offset + count
-        self.set_offset(new_off)
-        
-        return messages
-
     def connect(self, token):
-        self._session = observ2.establish_session(self._service)
-        self._session.login(self._account, token)
-        result = self._session.select(constants.INBOX, readonly=True)
-        return result
+        """
+
+        ->
         
+        Function returns the number of messages in the folder.
+        """
+        self._session = observ2.establish_session(self._service)
+        status = self._session.login(self._account, token)
+        print(status)
+        count = self.count_messages()
+        print(count)
+        
+        return count
+
+    def count_messages(self, folder=constants.INBOX):
+        """
+
+        -> int
+        
+        Function returns the number of messages in the current folder. 
+        """
+        response = self._session.select(folder, readonly=READ_ONLY)
+        count_string = unpack_response(response)
+        result = int(count_string)
+        return result
+
     def disconnect(self):
         pass
 
-    def refresh(self):
-        serials = self._get_serials()
-        uniques = self._get_uniques(serials)
-        self._uniques = uniques
-        # bad structure
-        return uniques
-    
-    def _build_list(self, serials):
-        uniques = list()
-        for serial in serials:
-            unique = self._get_uid(serial)
-            uniques.append(unique)
-        return uniques
-    
-    def _get_message(self, uid):
+    def get_message(self, uid):
+        """
+
+        -> returns message obj
+        
+        """
         message = observ2.get_message_by_UID(self._session, uid)
         return message
 
+    def get_messages(self, offset=None, count=BATCH, trace=False):
+        """
+
+        Function returns a list of messages of up to length count, starting at
+        the offset. You can use negative offsets.
+        """
+        messages = list()
+        if offset is None:
+            # I want to keep the data if offset=0
+            offset = self.get_offset()
+        uniques = self.refresh(offset, count, increment=True)
+        for uid in uniques:
+            message = self.get_message(uid)
+            messages.append(message)
+        
+        return messages
+
+    def get_new(self):
+        pass
+        # checks new, returns news
+
+    def get_offset(self):
+        result = self._offset
+        return result
+
+    def increment_offset(self, number):
+        new = self.get_offset() + number
+        self.set_offset(new)
+
+    def refresh(self, offset, count, increment=True):
+        """
+
+        -> list
+
+        Return a list of uniques for emails after the offset
+        """
+        serials = self._get_serials()
+        # returns all serials
+
+        positioned = serials[offset:]
+        sized = positioned[:count]
+        
+        uniques = self._get_uniques(sized)
+        # Think about storing uniques, or storing a map of the uniques or
+        # something? map is: serial: unique, serial: unique; omit serials where
+        # unique is last unique plus serial - last serial. The purpose of this
+        # map would be to figure out what's going on if get new messages?
+
+        if increment:
+            length = len(uniques)
+            self.increment_offset(length)
+        
+        return uniques
+
+    def set_offset(self, number):
+        self._offset = number
+
     def _get_serials(self, trace=False):
-        serials = observ2.get_ids(self._session, trace=trace)
+        """
+
+        _get_serials() -> list
+
+        Function returns a list of strings for all of the messages in the inbox.
+        You have to use strings to pass in ids.       
+        """
+        response = self._session.search(None, ALL)
+        serial_string = unpack_response(response)
+        serials = serial_string.split()
+        # string looks like this: '1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16',
+        # consists of integers separated by spaces.
+        
         return serials
 
     def _get_uid(self, serial_number):
@@ -101,14 +171,19 @@ class ImapConnector:
             result.append(unique)
 
         return result
-        
-class EmailObserver:
-    def __init__(self):
-        self.connector = ImapConnector()
-        self.worker = Welder()
-        
-    # something about namespaces?
-    # define the main methods of check() and construct(), plus nonpublics.
+
+def unpack_response(response):
+    """
+
+    unpack_response() -> string
+
+    Function expects a response in the form of ("resp_code", [b'blah blah']). 
+    You get the string back, but still need to parse it.
+    """
+    resp, list_of_one_bytestring = response
+    bytestring = list_of_one_bytestring[0]
+    string = bytestring.decode()
+    return string
     
 # Testing
 url = constants.GMAIL
@@ -117,14 +192,14 @@ def run_test(url):
     # The EmailObserver should pass those in. The Controller should send them
     # to EmailObserver.
     ct1 = ImapConnector(url=url, guest=guest)
-    result = ct1.connect(token)
-    print(result)
+    print(ct1)
+    count = ct1.connect(token)
+    print("Count: ", count)
 
-    uniques = ct1.refresh()
-    print("First 10: ", uniques[:10])
-    print("Last 10:  ", uniques[-10:])
+    uniques1 = ct1.refresh(offset=-30, count=10)
+    print("Uniques #1: ", uniques1)
 
-    msgs = ct1.check()
+    msgs = ct1.get_messages()
     print(msgs)
     
     return msgs
@@ -136,4 +211,10 @@ if __name__ == "__main__":
 # need a routine to go from uid to serial, so i can start looking up from there.
 #   then on refresh, i check what position the last unique is in, and get new
 #   from there ids from beyond that position.
+
+# wishlist:
+#  get uniques on first check, or in batches instead of serials
+#  retrieve multiple emails at once.
+#
+# i should be passing in offset and batch size during activation
 
